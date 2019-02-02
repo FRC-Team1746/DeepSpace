bc218319f7f17a59b1fe377f3d752751673e70ae
Sarah Storer <sstorer3@gatech.edu>
2019-02-02 13:11:14 -0500

    back to basics

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -1,46 +1,49 @@
-
 package frc.robot;
 
-import edu.wpi.first.wpilibj.TimedRobot;
+import com.ctre.phoenix.motorcontrol.*;
+import com.ctre.phoenix.motorcontrol.can.*;
+import edu.wpi.first.wpilibj.*;
 
 public class Robot extends TimedRobot {
-  
-  Controls m_controls;
-  TeleopDrive m_TeleopDrive;
-
-  @Override
-  public void robotInit() {
-    m_controls = new Controls();
-    m_TeleopDrive = new TeleopDrive(m_controls);
-    m_TeleopDrive.resetGyro();
-    m_TeleopDrive.resetEncoders();
-  }
-
-
-  @Override
-  public void autonomousInit() {
-    
-  }
-
-
-  @Override
-  public void autonomousPeriodic() {
-    
-  }
+  TalonSRX _talon = new TalonSRX(0); /* make a Talon */
+  Joystick _joystick = new Joystick(0); /* make a joystick */
+  Faults _faults = new Faults(); /* temp to fill with latest faults */
 
   @Override
   public void teleopInit() {
-		m_TeleopDrive.setRampRate(.5);
-		m_TeleopDrive.setBrakeMode(false);
+    /* factory default values */
+    _talon.configFactoryDefault();
+
+    /*
+     * choose whatever you want so "positive" values moves mechanism forward,
+     * upwards, outward, etc...
+     *
+     * Note that you can set this to whatever you want, but this will not fix motor
+     * output direction vs sensor direction.
+     */
+    _talon.setInverted(false);
+
+    /*
+     * flip value so that motor output and sensor velocity are the same polarity. Do
+     * this before closed-looping
+     */
+    _talon.setSensorPhase(false); // <<<<<< Adjust this
   }
 
   @Override
   public void teleopPeriodic() {
-		m_TeleopDrive.teleopArcadeDrive();
-		// updateSmartDashboard();
-  }
+    double xSpeed = _joystick.getRawAxis(1) * -1; // make forward stick positive
 
-  @Override
-  public void testPeriodic() {
+    /* update motor controller */
+    _talon.set(ControlMode.PercentOutput, xSpeed);
+    /* check our live faults */
+    _talon.getFaults(_faults);
+    /* hold down btn1 to print stick values */
+    if (_joystick.getRawButton(1)) {
+      System.out.println("Sensor Vel:" + _talon.getSelectedSensorVelocity());
+      System.out.println("Sensor Pos:" + _talon.getSelectedSensorPosition());
+      System.out.println("Out %" + _talon.getMotorOutputPercent());
+      System.out.println("Out Of Phase:" + _faults.SensorOutOfPhase);
+    }
   }
 }
